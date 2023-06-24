0fec7782a43e515258cc4df8383286f173955978
yulianadewi18 <yulianadewi210@gmail.com>
2023-06-24 22:58:11 +0800

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://exercise-galleryapp-dewi.netlify.app/" // TODO: replace this
+const studentName = "Yuliana Dewi" // TODO: replace this
+const studentId = "FE4644682" // TODO: replace this
+
+export { url, studentName, studentId }
